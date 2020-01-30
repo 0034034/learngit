b9b7b144aaea3dd5a0dc6f804a4a07ab77357a4b
root <0034034@sina.com>
2020-01-30 23:27:51 +0800

    add user.py

---
+++ awesome-python3-webapp/www/User.py
@@ -0,0 +1,16 @@
+from orm import Model,StringField, IntegerField
+import asyncio,logging
+import aiomysql
+
+class User(Model):
+    __table__ = 'users'
+    id = IntegerField(primary_key = True)
+    name = StringField()
+
+user = User(id=123,name='Michael')
+yield from user.save()
+users = User.findAll()
+print(user['id'])
+print(user.id)
+
+aaa
